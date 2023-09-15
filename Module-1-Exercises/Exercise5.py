b6a8590274c6b9dd7ddc253dcac2581b9b19c9b1
Ramon <supersplendid456@gmail.com>
2023-09-15 12:48:45 +0300

    Commit 8 (for Exercise 5)

--- Module-1-Exercises/Exercise5.py
+++ Module-1-Exercises/Exercise5.py
@@ -35,5 +35,61 @@ for x in range(dice_to_roll):
     dice_rolls.append(y)
 print(f"Sum of the dice rolled was {sum(dice_rolls)}")
 
+# write a program that asks the user to enter numbers until they input an empty string to quit
+# at the end, the program prints out 5 greatest number in desc. order
+# you can reverse the order of list items by doing sort w/ reverse=true argument
 
+largestnumbers = []
+while True:
+    num = input("Enter a number : ")
+    if num == "":
+        break
+    try:
+        num = int(num)
+        largestnumbers.append(num)
+    except:
+        print("Invalid input")
+        continue
 
+print("The 5 largest numbers in descending order is : ")
+print(sorted(largestnumbers[-5:], reverse=True))
+
+# write a program that asks the user for an integer and tells if the number is a prime number
+# prime numbers are numbers that are only divisible by one or itself
+
+primenumber = int(input("Enter a number to see if it's a prime number: "))
+
+flag = False
+
+if primenumber ==1:
+    print(primenumber, "is not a prime number")
+elif primenumber >1:
+    # checking for factors
+    for i in range(2, primenumber):
+        if (primenumber % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            break
+    if flag:
+        print(primenumber, "is not a prime number")
+    else:
+        print(primenumber, "is a prime number")
+
+# look at for loops a bit more
+
+# write a program that asks the user for names of 5 cities one by one and puts em in list
+# program prints out names one by one, one city per line, in the same order as input
+# use for loop to read names, ask for names and for/in to iterate through the list
+
+cities = []
+city_entered = 0
+
+while city_entered < 5:
+    cityname = input("Enter a name of a city: ")
+    cities.append(cityname)
+    city_entered = city_entered + 1
+else:
+    print("Printing names...")
+
+for i in range(0, len(cities)):
+    print(cities[i])

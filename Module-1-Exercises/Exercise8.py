3ec5169ccd5d54226eaf925316aab754ade1c7b3
Ramon <supersplendid456@gmail.com>
2023-10-10 15:49:24 +0300

    Exercise 8 commit

--- Module-1-Exercises/Exercise8.py
+++ Module-1-Exercises/Exercise8.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from geopy import distance
 
 connection = mysql.connector.connect(
     host='127.0.0.1',
@@ -23,7 +24,6 @@ def GetAirportByICAO(icao):
     return
 
 
-
 def GetAirportByArea(area):
     sql = "SELECT type, COUNT(type) FROM airport"
     sql += " WHERE iso_country = '" + area + "' "
@@ -39,8 +39,31 @@ def GetAirportByArea(area):
     return
 
 
+def GetAirportDistance(icao):
+    sql = " select country.name, ident, airport.name, latitude_deg, longitude_deg from airport, country "
+    sql += " where airport.iso_country = country.iso_country and ident = '" + icao + "'"
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    return result
+
+
+def airport_distance(first, second):
+    start = GetAirportDistance(first)
+    ending = GetAirportDistance(second)
+    start_coordinates = (start[0]['latitude_deg'], start[0]['longitude_deg'])
+    end_coordinates = (ending[0]['latitude_deg'], ending[0]['longitude_deg'])
+
+    return int(distance.distance(start_coordinates, end_coordinates).km)
+
+
 icao = input("Enter an airport ICAO code : ")
 GetAirportByICAO(icao)
 
 area = input("Enter an area code to fetch amount of airports by country: ")
 GetAirportByArea(area)
+
+airport_1 = input("Enter an ICAO code for the first airport : ")
+airport_2 = input("Enter an ICAO code for the second airport : ")
+print(f"THe distance between the airports inputted is {airport_distance(airport_1, airport_2)} km.")
+
